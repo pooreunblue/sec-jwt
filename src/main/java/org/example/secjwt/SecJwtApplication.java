6f23b1f7274337900527193d23771b118ee35353
bsh <mjbsh02@mju.ac.kr>
2026-08-19 09:12:04 +0900

    Initial commit

---
+++ src/main/java/org/example/secjwt/SecJwtApplication.java
@@ -0,0 +1,13 @@
+package org.example.secjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/secjwt/SecJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
